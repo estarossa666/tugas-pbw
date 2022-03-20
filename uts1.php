94fc4a8ba67851b447e38f3ba4b5121a7b50dc8d
estarossa666 <2010631170081@student.unsika.ac.id>
2022-03-21 00:02:04 +0700

    UTS

---
+++ uts1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Variabel</title>
+</head>
+<body>
+<h2>Menggunakan Variabel</h2>
+
+<?php
+
+$varstring = 'Variabel Bertipe String';
+$varfloat = 3.14;
+$varinteger = 212;
+
+//mencetak nilai variabel
+echo #varstring."<br/>";
+echo #varfloat."<br/>";
+echo #varinteger;
+
+?>
+</body>
+
+</html>
